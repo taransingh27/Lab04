8ed5b92f297ef5032545e3fdd72e8f06f69d15d3
taransingh27 <taran.singh_cs18@gla.ac.in>
2019-09-04 15:18:52 +0530

    used toString method to get output:Main.java

---
+++ src/lab04/Main.java
@@ -0,0 +1,11 @@
+package lab04;
+
+public class Main {
+    public static void main(String[] args) {
+        Person p1 = new Person("Taran", 18, 'm');
+        System.out.println(p1.toString());
+        System.out.println(p1);
+
+
+    }
+}
