c6b809df3ac9cded08ebaa7ea2c6879d2623be4a
taransingh27 <taran.singh_cs18@gla.ac.in>
2019-09-04 14:40:16 +0530

    created getter and setter method for fields:Person.java

--- src/lab04/Person.java
+++ src/lab04/Person.java
@@ -5,5 +5,31 @@ public class Person {
     private int age;
     private char gender;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setAge() {
+        this.age = age;
+    }
+
+    public void setGender() {
+        this.gender = gender;
+    }
+
+
+
 
 }
